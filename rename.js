d3d540bda68bb6ce1c26ea556bda96f7ba3bcb80
Jazcash <Jazcash@users.noreply.github.com>
2014-06-26 12:52:28 +0200

    Full commit

--- rename.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs")
-
-fs.readdirSync("resources/webms").forEach(function(filename) {
-	if (filename == ".svn") return;
-	var oldfilename = "resources/webms/"+filename
-	var newfilename = oldfilename.substr(0, oldfilename.length-5)
-	fs.renameSync(oldfilename, newfilename)
-});
